ee79590f200a9638f62696d21396bf165a6537f6
pizhou <1983746045@qq.com>
2019-11-07 23:26:35 +0800

    Swagger的添加

---
+++ manager/src/main/java/com/imooc/manager/configurations/SwaggerConfiguration.java
@@ -0,0 +1,34 @@
+package com.imooc.manager.configurations;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+    @Bean
+    public Docket ControllerApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .build();
+
+
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("理财系统")
+                .description("管理端接口")
+                .version("2.0")
+                .build();
+    }
+
+}
